3f4b26412d75be853ef826b2cea0c7fb3400c030
main12sani <main12sani@gmail.com>
2021-07-30 23:12:38 +0600

    array slice

---
+++ array-slice.js
@@ -0,0 +1,15 @@
+// array slice, splice, join
+
+// array slice
+
+let num_list = [0, 1, 2, 3, 4, 5, 6, 7];
+
+console.log(num_list);
+
+console.log(num_list.slice(0, 2));
+
+// console.log(num_list.splice(0, 4));
+
+
+// console.log(num_list.join(" is a number"+" "+" ,"));
+
