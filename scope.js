2214ffeab9446c96ffb8a98300dc0ff972956920
main12sani <main12sani@gmail.com>
2021-07-30 22:32:07 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,16 @@
+var array_num = [1, 2, 3, 4, 5, 6, 7];
+
+
+
+function sum(num1, num2) {
+    let sum_result = num1 + num2;
+    if (sum_result > 10) {
+        let days = "Good Morning";
+        // console.log(days);
+    }
+    // console.log(days);
+    console.log(array_num);
+}
+// console.log(sum_result);
+let result = sum(10, 20);
+console.log(result);
